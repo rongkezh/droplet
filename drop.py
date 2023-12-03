5b2eaf7da72d2cf423e4d440cfa8fc7a02e50c45
Rongke <zrongke@gmail.com>
2023-12-02 17:07:08 -0800

    waterdrop.py functional

--- drop.py
+++ drop.py
@@ -1,40 +1,23 @@
-# main class
-# TODO: UI
+# testing purpose only
+
+# testing direction with board 2d array
 import random
-# 
-class Game:
-    def __init__(self):
-        self.gameboard = {}
-        msg = "Welcome to droplets game."
-        print(msg)
-        # game status. False if game is finished.
-        self.on = True
-    def off(self):
-        self.on = False
-class Board:
-    def __init__(self, length, difficulty):
-        self.length = length
-        # TODO: difficulty level should affect the map generation.
-        self.map = [[random.randint(0,9) for i in range(length)] for j in range(length)]
-    def printmap(self):
-        for row in self.map:
-            for elem in row:
-                print(elem, end=' ')
-            print()
-
-# drops at grids
-class Drop:
-    def __init__(self,x,y,size):
-        self.x = x
-        self.y = y
-        self.size = size
-
-    def __str__(self):
-        return f"({self.x},{self.y}){self.size}"
-
-    def add_water(self,x,y):
-        self.size += 1
-        if self.size > 9:
-            self.size = 0
-            # TODO: splash to four directions; if no drop in any direction, splash would bounce and diminish on the boarder.
-
+pop_size = 9
+size = 5
+board = [[random.randint(0, pop_size-1) for j in range(size)] for i in range(size)]
+def printboard():
+    for i in range(size):
+        for j in range(size):
+            print(f'{board[i][j]} ', end='')
+        print()
+row = 1
+col = 1
+index = board[row][col]
+printboard()
+print(f'{index}')
+direction = [-1,0]
+rowadd,coladd = direction
+print(rowadd)
+print(coladd)
+index = board[row+rowadd][col+coladd]
+print(index)
--- waterdrop.py
+++ waterdrop.py
@@ -2,8 +2,12 @@ import random
 
 class Board:
     def __init__(self, size, pop_size):
+        # size is the number of spaces per row or per col.
+        # pop_size is the max for a water cell. when pop_size =  3, if a cell is larger than 3, it pops
+
         self.size = size
         self.pop_size = pop_size
+        # randomize the board of cells.
         self.board = [[random.randint(0, pop_size-1) for j in range(size)] for i in range(size)]
     
     def print_board(self):
@@ -12,59 +16,85 @@ class Board:
                 print(f'{self.board[i][j]} ', end='')
             print()
     
-    def add_water(self, row, col, amount):
-        if self.board[row][col] + amount > self.pop_size:
-            overflow = self.board[row][col] + amount - self.pop_size
-            self.board[row][col] = self.pop_size
+    def add_water(self, row, col):
+        # add drop to cell at row,col. TODO:return the overflow of drops
+        self.board[row][col] += 1
+        if self.board[row][col]>= self.pop_size:
+            #overflow = self.board[row][col] - self.pop_size
             self.pop(row, col)
-            return overflow
+            #return overflow
+            return
         else:
-            self.board[row][col] += amount
-            return 0
+            return
     
     def pop(self, row, col):
-        drops = self.board[row][col] - 1
         self.board[row][col] = 0
-        for r, c in [(row-1,col), (row+1,col), (row,col-1), (row,col+1)]:
-            if r >= 0 and r < self.size and c >= 0 and c < self.size:
-                if self.board[r][c] > 0:
-                    self.board[r][c] += 1
+        #TODO: make water travel in four directions.
+        #for direction in ((row-1,col), (row+1,col), (row,col-1), (row,col+1)):
+        # cells at up, down, left, right, add_water.
+        for r,c in [(-1,0), (+1,0), (0,-1), (0,+1)]:
+            # check if out of bounds
+            row1=row+r
+            col1=col+c
+            if (row1) >= 0 and (row1) < self.size and (col1) >= 0 and (col1) < self.size:
+                if self.board[row1][col1] > 0:
+                    self.add_water(row1,col1)
                 else:
-                    self.board[r][c] = 1
-                    self.splash(r, c, drops)
-    
-    def splash(self, row, col, drops):
-        if drops <= 0:
-            return
-        for r, c in [(row-1,col), (row+1,col), (row,col-1), (row,col+1)]:
-            if r >= 0 and r < self.size and c >= 0 and c < self.size:
-                if self.board[r][c] > 0:
-                    self.board[r][c] += 1
-                else:
-                    self.board[r][c] = 1
-                    self.splash(r, c, drops-1)
-
+                    self.splash(row1, col1, r, c)
+    def splash(self, row, col, r, c):
+        row1=row+r
+        col1=col+c
+        if (row1) >= 0 and (row1) < self.size and (col1) >= 0 and (col1) < self.size:
+            # check if out of bounds
+            if self.board[row1][col1] > 0:
+                self.add_water(row1,col1)
+            else:
+                self.splash(row1, col1, r, c)            
 
 class Game:
-    def __init__(self, size=5, pop_size=3, water=100):
+    def __init__(self, size=5, pop_size=5, water=100):
+        # water is the number of drops player has in the game
         self.board = Board(size, pop_size)
         self.water = water
         self.rounds = 0
+        print("Welcom to Droplet game!")
+        print(f"On this {size} by {size} board, each cell can hold up to {pop_size} drops of water before it pops.")
+        print("Try to pop every cell. Good luck!")
+        print(f"You start with {water} number of drops.")
     
     def play(self):
         while self.water > 0:
             self.board.print_board()
             print(f'You have {self.water} drops of water.')
-            row = int(input('Enter row: '))
-            col = int(input('Enter col: '))
-            amount = 1
-            overflow = self.board.add_water(row, col, amount)
-            self.water -= amount - overflow
+            print('Please enter where to place your next drop of water.')
+            # take the row and col numbers and raise exception if not entered in valid form
+            row = self.get_input('row')
+            col = self.get_input('col')
+            overflow = self.board.add_water(row, col)
+            # TODO: add overflow to self.water number
+            # self.water -= 1- overflow
             self.rounds += 1
+            self.water -= 1
             if self.check_win():
-                print('You win!')
+                print(f'You win! Total rounds: {self.rounds}')
                 return
-        print('You lose!')
+        print(f'You lose! Total rounds: {self.rounds}')
+
+    def get_input(self,prompt):
+        while True:
+            print('Enter', prompt,':', end = " ")
+            try:
+                index = int(input())-1
+            except ValueError:
+                print(f"Please enter a integer between 1 and {self.size}")
+                continue
+            if index < 0 or index >= self.size:
+                print(f"Please enter a integer between 1 and {self.size}")
+                continue
+            else:
+                break
+        return index
+
     
     def check_win(self):
         for i in range(self.board.size):
